5dcbbd39bffa25f7fcb3e40ff3ab7d89abdced93
Christopher G <135375609+AI-Ocko@users.noreply.github.com>
2025-02-28 23:51:20 -0800

    Working version of sphere calculator

--- Main.cpp
+++ Main.cpp
@@ -6,13 +6,23 @@ using namespace std;
 
 int main() {
 
+
+    //declarations
     double radiusTemp;
     
+    Sphere basketball(2.0);
+
+    // get user input
+    cout << "What is the radius for the basketball: " << endl;
+    cin >> radiusTemp;
+    basketball.setRadius(radiusTemp);
 
 
-
-
-
+    //calculation and output
+    radiusTemp = basketball.getRadius();
+    cout << "Basketball: " << endl;
+    cout << "Surface area: " << basketball.calcSurfaceArea(radiusTemp);
+    cout << "Volumne: " << basketball.calcVolume(radiusTemp);
 
 
 
